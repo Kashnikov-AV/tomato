3fe0cb364d300d5188d466e122c08c60a5dceb10
ivan <egkashnikov@gmail.com>
2025-03-13 23:18:54 +0300

    fix

--- soil/settings.py
+++ soil/settings.py
@@ -154,8 +154,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static', ]
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [BASE_DIR / 'static', ]
 
 # Base url to serve media files
 MEDIA_URL = 'media/'
